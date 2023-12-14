8c65783fdcd2b0f51c203d091f2f27c4fb6d6fe1
ARcc <dmitry.yudin.a@gmail.com>
2023-12-14 09:09:51 +0100

    upgrade to newer version of java and spring framework

--- src/main/java/excel/cli/Main.java
+++ src/main/java/excel/cli/Main.java
@@ -4,7 +4,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
+import java.util.HashMap;
 import java.util.List;
 
 import excel.converter.Actions;
@@ -13,6 +13,7 @@ import excel.converter.XLSX2XMLConverter;
 import excel.converter.XML2XLSXConverter;
 
 public class Main {
+    private static final String actionGetXlsxFromXml = "GET_XLSX_FROM_XML";
 
     public static void main(String[] args) {
         new Main().run(args);
@@ -20,53 +21,96 @@ public class Main {
     }
 
     private void run(String[] args) {
+        if (args.length == 0) {
+            System.out.println("you need to provide some parameters");
+            return;
+        }
+        if ((args.length % 2) != 0) {
+            System.out.println("you need to provide parameters in pairs ");
+            return;
+        }
+
+        var parameters = this.getParametersDictionary(args);
 
         try {
 
-            Path path = Paths.get(args[0]);
-            List<String> parametersList = Files.readAllLines(path, StandardCharsets.UTF_8);
-            System.out.println(parametersList.size());
-            for (int i = 0; i < parametersList.size(); i++) {
-                System.out.println(parametersList.get(i));
+            // get the action
+            var action = parameters.get("--action");
+            if (action == null) {
+                System.out.println("action must be defined with --action parameter");
+                return;
             }
 
-            // depending on the action instantiate different files
-            String action = parametersList.get(1);// the first argument is always action
-            if (Actions.GET_XLSX_FROM_XML.toString().equals(action)) {
+            switch (action) {
+                case actionGetXlsxFromXml:
 
-                // we are requested to translate the XMl file into XLSX document. instantiate
-                // the translator
-                // define source folder
+                    String sourceXML = parameters.get("--source");
+                    String targetExcel = parameters.get("--target");
 
-                String sourceXML = parametersList.get(2);
-                String targetExcel = parametersList.get(3);
+                    XML2XLSXConverter xml2XLSXConverter = XML2XLSXConverter.createConverter(sourceXML, targetExcel);
+                    xml2XLSXConverter.convert();
+                    break;
 
-                XML2XLSXConverter xml2XLSXConverter = XML2XLSXConverter.createConverter(sourceXML, targetExcel);
-                xml2XLSXConverter.convert();
-            } else if (Actions.GET_XML_FROM_XLSX.toString().equals(action)) {
-
-                // we are requested to translate the xlsx file into xml document. instantiate
-                // the right translator
-                // define source file
-                String sourceFile = parametersList.get(2);
-                String targetFile = parametersList.get(3);
-
-                XLSX2XMLConverter xlsx2XMLConverter = XLSX2XMLConverter.createConverter(sourceFile, targetFile);
-                xlsx2XMLConverter.convert();
-
-            } else if (Actions.GENERATE_PICTURES.toString().equals(action)) {
-                // we need to generate the pictures based on the file
-                String sourceFile = parametersList.get(2);
-                String targetPath = parametersList.get(3);
-
-                PictureParser pictureParser = new PictureParser();
-                pictureParser.convert(sourceFile, targetPath);
+                default:
+                    break;
             }
-
+            /*
+             * // depending on the action instantiate different files
+             * String action = parametersList.get(1);// the first argument is always action
+             * if (Actions.GET_XLSX_FROM_XML.toString().equals(action)) {
+             * 
+             * // we are requested to translate the XMl file into XLSX document. instantiate
+             * // the translator
+             * // define source folder
+             * 
+             * String sourceXML = parametersList.get(2);
+             * String targetExcel = parametersList.get(3);
+             * 
+             * XML2XLSXConverter xml2XLSXConverter =
+             * XML2XLSXConverter.createConverter(sourceXML, targetExcel);
+             * xml2XLSXConverter.convert();
+             * } else if (Actions.GET_XML_FROM_XLSX.toString().equals(action)) {
+             * 
+             * // we are requested to translate the xlsx file into xml document. instantiate
+             * // the right translator
+             * // define source file
+             * String sourceFile = parametersList.get(2);
+             * String targetFile = parametersList.get(3);
+             * 
+             * XLSX2XMLConverter xlsx2XMLConverter =
+             * XLSX2XMLConverter.createConverter(sourceFile, targetFile);
+             * xlsx2XMLConverter.convert();
+             * 
+             * } else if (Actions.GENERATE_PICTURES.toString().equals(action)) {
+             * // we need to generate the pictures based on the file
+             * String sourceFile = parametersList.get(2);
+             * String targetPath = parametersList.get(3);
+             * 
+             * PictureParser pictureParser = new PictureParser();
+             * pictureParser.convert(sourceFile, targetPath);
+             * }
+             */
         } catch (Exception e) {
             e.printStackTrace();
         }
         // the first parameter should be action
 
+    }
+
+    private HashMap<String, String> getParametersDictionary(String[] args) {
+        var parametersDictionary = new HashMap<String, String>();
+        var parameterName = "";
+        var parameterValue = "";
+        for (int i = 0; i < args.length; i++) {
+            if (i % 2 == 0) {
+                // this is the name of parameter
+                parameterName = args[i];
+            } else {
+                // this is the value of parameter
+                parameterValue = args[i];
+                parametersDictionary.put(parameterName, parameterValue);
+            }
+        }
+        return parametersDictionary;
     }
 }
