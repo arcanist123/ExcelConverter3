0d8c5a6b6a1d9d3bf8a48c8c903ab261bc61e654
arcanist123 <dmitry.yudin.a@gmail.com>
2023-12-28 21:37:26 +0100

    Updated parser

--- src/main/java/excel/adapters/JsonToDomainAdapter.java
+++ src/main/java/excel/adapters/JsonToDomainAdapter.java
@@ -1,6 +1,7 @@
 package excel.adapters;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import excel.domains.Document;
 
@@ -13,7 +14,12 @@ public class JsonToDomainAdapter {
 
     public Document getDocument() {
         JSONArray ja = new JSONArray(json);
-        
+        var length = ja.length();
+
+        for (Object material : ja) {
+            var jo = new JSONObject(material.toString());
+var aaa = jo.getJSONObject("material");
+        }
         return null;
     }
 
